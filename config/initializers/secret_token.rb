09670a530cf56682dc9992826c9d7f3cae8c4fc7
Pete <petevonna@lycos.com>
2013-01-19 16:19:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Davepowerman::Application.config.secret_token = 'c574d72cf1391fdb4ead8a46ef44a0c296b29fa040709c001e053528ba79f670a4e725404477c0f330f63aa066ec7955cf5f5cd8bad9dc61c8c09312779ab8f1'
